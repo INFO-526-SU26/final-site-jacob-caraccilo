567fa97fc2a0d4b3563ce8e9d66cbc369ec56b94
Jacob Caraccilo <jcaraccilo032400@gmail.com>
2026-06-28 15:56:01 -0700

    Updated r script for combining census data to also remove Puerto Rico and rows lacking data in the water_insecurity datasets. Also committing updated csv file names.

--- _scripts/clean_water_data_and_combine_with_census_data.R
+++ _scripts/clean_water_data_and_combine_with_census_data.R
@@ -6,6 +6,30 @@ water_2023 <- read_csv("data/water_insecurity_2023.csv")
 census_2022 <- read_csv("data/census_2022.csv")
 census_2023 <- read_csv("data/census_2023.csv")
 
+# removes Puerto Rico from the water insecurity datasets
+water_2022 <- water_2022 |>
+  filter(!str_starts(geoid, "72"))
+water_2023 <- water_2023 |>
+  filter(!str_starts(geoid, "72"))
+
+# removes Bartow County, GA and Lafourche Parish, LA from the water insecurity datasets
+# for whatever reason, these two county / county equivalent areas were in the
+# dataset despite lacking plumbing data
+water_2022 <- water_2022 |>
+  filter(
+    !name %in% c(
+      "Lafourche Parish, Louisiana",
+      "Bartow County, Georgia"
+    )
+  )
+water_2023 <- water_2023 |>
+  filter(
+    !name %in% c(
+      "Lafourche Parish, Louisiana",
+      "Bartow County, Georgia"
+    )
+  )
+
 # combines the 2022 water insecurity and Census datasets together based on rows
 # where geoid matches, which is the county identifier
 water_census_2022 <- left_join(
